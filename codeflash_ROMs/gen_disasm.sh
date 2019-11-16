363ccb61a9129bd783ce8011859f4e6153341067
Kris Bahnsen <Kris@KBEmbedded.com>
2019-11-16 13:14:50 -0800

    ROMs: Add script to generate disassemblies
    Will walk each subdir, find the first codeflash file, split it in
    to 16 KiB parts, then disassemble each part
    
    These disasm/ folders are ignored by git

---
+++ codeflash_ROMs/gen_disasm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: BSD-2-Clause
+
+# This script will take the first file with the name "*codeflash*.bin" in each
+# subdirectory, split it in to 16 KiB pages, and disassemble each page using
+# z80dasm.
+# Aside from the first page of CF, which is hardwired to 0x0000 base, all of the
+# other CF pages should only ever be mapped to 0x4000 base, aka slot4000.
+
+for DIR in */; do
+(
+  cd "${DIR}" || exit
+  mkdir disasm
+  FILE=$(ls ./*codeflash*.bin | head -n 1)
+  for I in $(seq -w 0 63); do
+    dd if="${FILE}" of=disasm/page"${I}".bin bs=16384 count=1 skip="${I}"
+    echo -e "; vim: syntax=z8a" > disasm/page"${I}".s
+    z80dasm -alt -g 0x4000 disasm/page"${I}".bin >> disasm/page"${I}".s
+  done
+  echo -e "; vim: syntax=z8a" > disasm/page00.s
+  z80dasm -alt -g 0x0 disasm/page00.bin >> disasm/page00.s
+)
+done
