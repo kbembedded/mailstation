51dba1b2abc0e6f96dd3374d3ea180afdfaca3e4
Kris Bahnsen <Kris@KBEmbedded.com>
2019-12-02 21:44:14 -0800

    src: FyOS: Removed use of src/common/ dir, not buildable
    Looks like sdcc is no longer able to build the existing fyos.c.
    I've verified this in the private project that for sure has been
    able to build in the past. This will require further investigation.

--- src/FyOS/v0.021/build.sh
+++ src/FyOS/v0.021/build.sh
@@ -11,6 +11,6 @@ sdcclib mslib.lib graphicslcd.rel
 sdcclib mslib.lib ms_plink.rel
 sdcclib mslib.lib mailstation.rel
 
-sdcc -mz80 --no-std-crt0 -o FyOS-v021.ihx --code-loc 0x8100 --data-loc 0x0000 -l mslib.lib ./crt0.rel ./fyos_c.c
+sdcc -mz80 --no-std-crt0 -o FyOS-v021.ihx --code-loc 0x8100 --data-loc 0x0000 -l mslib.lib ./crt0.rel ./fyos.c
 
 objcopy -Iihex -Obinary FyOS-v021.ihx FyOS-v021.bin
