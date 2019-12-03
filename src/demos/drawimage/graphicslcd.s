b9183f3ea94972616612902af75615a6a15d8b00
Kris Bahnsen <Kris@KBEmbedded.com>
2019-12-02 21:15:26 -0800

    src: Isolated FyOS, demos, tools common sources
    Decided that these examples are current snapshots in time, they
    may change and evolve, and the include files for them are not
    worth keeping in place and trying to magle later as this project
    evolves. As time moves on, bits and pieces of these will likely
    prove useful, but its not worth trying to maintain all of it at
    this point

--- src/demos/drawimage/graphicslcd.s
+++ src/demos/drawimage/graphicslcd.s
@@ -6,7 +6,7 @@
 ;//  the Mailstation firmware, just removed so
 ;//  that it can be accessed regardless of which
 ;//  firmware version is used (since the address
-;//  changes).  It draws the buffer at 0xc010 to 
+;//  changes).  It draws the buffer at 0xc010 to
 ;//  the LCD.
 ;//
 ;//  If you never plan to use it, comment it out
@@ -19,7 +19,7 @@ _UpdateGraphicsLCD::
 		push	bc
 		push	de
 		push	hl
-		
+
 		in	a, (#06)
 		ld	e, a
 		push	de
@@ -34,41 +34,41 @@ _UpdateGraphicsLCD::
 		pop	de
 		ld	a, e
 		out	(#06), a
-		
+
 		pop	hl
 		pop	de
 		pop	bc
 		pop	af
 		ret
-	
-	update_half:		
+
+	update_half:
 		ld	b, #0x14
 	update_half2:
 		ld 	de, #0x4038
-		
+
 		push	hl
 		ld	hl, (_p2shadow)
 		ld 	a, (hl)
-		and 	#0xF7      
+		and 	#0xF7
 		ld 	(hl),a
-		out 	(#02),a  		
-		
-		ld 	a, b      
-		dec 	a      
-		ld 	(de), a	
-		
+		out 	(#02),a
+
+		ld 	a, b
+		dec 	a
+		ld 	(de), a
+
 		ld	a, (hl)
 		or	#0x08
 		ld	(hl),a
-		out	(#02),a  
+		out	(#02),a
 		pop	hl
-		
+
 		ld	a, b
 		ld	b, #0x00
 		ld	c, #0x80
 		ldir
 		ld	b,a
-		djnz	update_half2		
-		ret         
+		djnz	update_half2
+		ret
 
 
