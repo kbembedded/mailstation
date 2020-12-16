1c9715d01df01c112df15282ac508fe24da61594
Kris Bahnsen <Kris@KBEmbedded.com>
2020-12-15 23:40:05 -0800

    FyOS-next: Lots of fiddling. Have figured out some quirks of msfw

--- src/demos/FyOS/next/FyOS-next.c
+++ src/demos/FyOS/next/FyOS-next.c
@@ -33,7 +33,7 @@ struct string_info {
 const struct string_hdr {
 	uint16_t str_cnt;
 	struct string_info str_info_list[12];
-	char name[9];
+	char name[10];
 	char menuF2[7];
 	char menuF3[10];
 	char menuF4[8];
@@ -84,15 +84,6 @@ const struct string_hdr {
 /* XXX: In order for this to be effective, need to solve the struct problem! */
 const struct menubar menudata[] = {
 	{STR_IDX_BACK, MENU_F1, SC_F1},
-	{STR_IDX_APP01, MENU_F2, SC_F2},
-	{STR_IDX_APP02, MENU_F3, SC_F3},
-	{STR_IDX_APP03, MENU_F4, SC_F4},
-	{STR_IDX_NEXT, MENU_F5, SC_F5},
-	{STR_IDX_PREV, MENU_F1_2, SC_F1},
-	{STR_IDX_APP04, MENU_F2_2, SC_F2},
-	{STR_IDX_APP05, MENU_F3_2, SC_F3},
-	{STR_IDX_APP06, MENU_F4_2, SC_F4},
-	{STR_IDX_APP07, MENU_F5_2, SC_F5},
 };
 
 /* XXX: I HAVE NO CLUE WHAT APPSTATE REALL DOES, IT APPEARS IT AFFECTS SIGNAL SOME HOW? */
@@ -104,66 +95,47 @@ const struct menubar menudata[] = {
  */
 uint16_t main(uint16_t appid, uint16_t appstate, uint16_t signal, uint16_t val1, uint16_t val2)
 {
-	char buf[21];
+	char buf[10];
 	/* This ends up in RAM not stack space */
-	static char textbuf[62];
-	static char textbuf2[62];
-	static uint8_t text_history_wid;
-	static uint8_t text_input_wid;
-	static uint8_t datebox_wid;
-	static uint8_t datestring_wid;
-	static char datestring[16]; // This length is arbitrary here
-
-	static uint8_t frombox_wid;
-	static uint8_t fromstring_wid;
-	static uint8_t fromstring[60]; // This length is arbitrary here
-
-	static uint8_t subjectbox_wid;
-	static uint8_t subjectstring_wid;
-	static uint8_t subjectstring[60]; // Length is arbitrary here
-
-	static uint8_t bodybox_wid;
-	static uint8_t bodystring_wid;
-	static uint8_t bodystring[512];
-
-	static uint8_t progbar;
-	struct menubar hellobar[5];
-	static unsigned int timeout;
-	static int timer;
+	static char histbuf[512];
+	static char linebuf[32];
+	static char histbox_wid;
+	static char bodybox_wid;
+	static char line_wid;
+	struct menubar fy_menu[1];
 
 
 	switch(signal) {
 	  case SIG_INIT: //sig_init
-		memset(textbuf, '\0', sizeof(textbuf));
+		/* Create menu bar at the bottom */
+		memcpy(fy_menu, menudata, sizeof(fy_menu));
+		msfw_menubar_set(appid, fy_menu, 1);
 
-		datebox_wid = msfw_widget_new(WID_RESOURCE, F_RES_STRING_L, 0xe, 0, 0x16, 9, appid, 5, 0x3c); //What is seqnum 5 in this case?
-		msfw_widget_event_handle(datebox_wid, S_TEXT_REFORMAT, 0, 0); // Reformat?
+		/* Draw foldertab/tile */
+		msfw_string_get_resource(STR_IDX_APP00, buf, sizeof(app_name.name));
+		msfw_draw_string_foldertab(buf);
 
-		strcpy(datestring, "4/20/69");
-		datestring_wid = msfw_widget_new(WID_TEXTENTRY, F_TEXT_HSCROLL, 0x24, 1, 0x50, 0x9, appid, 6, 0); // What is seqnum 6?
-		msfw_widget_event_handle(datestring_wid, S_TEXT_SETBUF, (uint16_t)datestring, sizeof(datestring)); // len?
-		msfw_widget_event_handle(datestring_wid, S_TEXT_REFORMAT, 0, 0); // Reformat?
-
-		frombox_wid = msfw_widget_new(WID_RESOURCE, F_RES_STRING_L, 0xB, 9, 0x19, 9, appid, 7, 0x4c); //seqnum?
-		msfw_widget_event_handle(frombox_wid, S_TEXT_REFORMAT, 0, 0); //reformat?
-
-		strcpy(fromstring, "arealEmail@aDDress.com");
-		fromstring_wid = msfw_widget_new(WID_TEXTENTRY, F_TEXT_HSCROLL, 0x024, 0xa, 0x11c, 9, appid, 8, 0);
-		msfw_widget_event_handle(fromstring_wid, S_TEXT_SETBUF, (uint16_t)fromstring, sizeof(fromstring));
-		msfw_widget_event_handle(fromstring_wid, S_TEXT_REFORMAT, 0, 0);
-
-		subjectbox_wid = msfw_widget_new(WID_RESOURCE, F_RES_STRING_L, 1, 0x12, 0x23, 9, appid, 9, 0x3D); //seqnum?
-		msfw_widget_event_handle(subjectbox_wid, S_TEXT_REFORMAT, 0, 0); //reformat?
-
-		strcpy(subjectstring, "A subject to remember");
-		subjectstring_wid = msfw_widget_new(WID_TEXTENTRY, F_TEXT_HSCROLL, 0x24, 0x13, 0x11c, 9, appid, 0xa, 0); //seqnum?
-		msfw_widget_event_handle(subjectstring_wid, S_TEXT_SETBUF, (uint16_t)subjectstring, sizeof(subjectstring));
-		msfw_widget_event_handle(subjectstring_wid, S_TEXT_REFORMAT, 0, 0);
-
-		/* I think this is the line across the screen */
-		bodybox_wid = msfw_widget_new(WID_RESOURCE, F_RES_BOX, 2, 0x1c, 0x13c, 1, appid, 0xB, 0);
+		/* Create simple bounding box for prettyness */
+		bodybox_wid = msfw_widget_new(WID_RESOURCE, F_RES_BOX, 2, 1, 0x13c, 0x55, appid, 5, 0); // Why 5? inbox app used this, not sure what this does
 		msfw_widget_event_handle(bodybox_wid, S_TEXT_REFORMAT, 0, 0); //reformat?
 
+		/* Put textbox inside of this */
+		memset(histbuf, '\0', sizeof(histbuf));
+		histbox_wid = msfw_widget_new(WID_TEXTENTRY, F_TEXT_MLINE_SPELL|F_TEXT_MLINE_CLIP|F_TEXT_RO, 3, 2, 0x13a, 0x4a, appid, 6, 0);
+		msfw_widget_event_handle(histbox_wid, S_TEXT_SETBUF, (uint16_t)histbuf, sizeof(histbuf));
+		msfw_widget_event_handle(histbox_wid, S_TEXT_REFORMAT, 0, 0);
+		msfw_widget_event_handle(histbox_wid, S_TEXT_UKN1, 1, 0); // Make RO?
+
+		/* Input box */
+		memset(linebuf, '\0', sizeof(linebuf));
+		line_wid = msfw_widget_new(WID_TEXTENTRY, F_TEXT_ALPHA|F_TEXT_HSCROLL, 3, 0x4b, 0x13a, 0x9, appid, 7, 0); // is HSCROLL needed? 
+		msfw_widget_event_handle(line_wid, S_TEXT_SETBUF, (uint16_t)linebuf, sizeof(linebuf));
+		msfw_widget_event_handle(line_wid, S_TEXT_REFORMAT, 0, 0);
+		msfw_widget_focus_set(line_wid);
+
+
+
+#if 0
 		strcpy(bodystring, "Test\r\nI wonder if the box prefers return vs newline\r\nor if it doesn't care and just wants to wordwrap on its own. So here is a nice long line to let that happen\r\n\r\nThat was two returns.\r\n\r\nthis was two newlines. You know I never tried \r\nfor return newline.\r\nLets\r\nMake\r\nSome\r\nSCROLLL");
 		bodystring_wid = msfw_widget_new(WID_TEXTENTRY, F_TEXT_MLINE_SPELL|F_TEXT_MLINE_CLIP|F_TEXT_RO, 2, 0x1e, 0x13c, 0x4a, appid, 0xC, 0);
 		msfw_widget_event_handle(bodystring_wid, S_TEXT_SETBUF, (uint16_t)bodystring, sizeof(bodystring));
@@ -174,7 +146,6 @@ uint16_t main(uint16_t appid, uint16_t appstate, uint16_t signal, uint16_t val1,
 		msfw_widget_event_handle(bodystring_wid, 0x22, 4, 0);
 		msfw_widget_event_handle(bodystring_wid, 0x22, 4, 0);
 		msfw_widget_focus_set(bodystring_wid);
-#if 0
 
 New Widget, type: 0x0003, flags: 0x0800, left: 0x000E, top: 0x0000, width: 0x0016, height: 0x0009, appid: 0x0001, seqnum: 0x0005, str_etc: 0x003C
 Widget event, handle: 0x0000, signal: 0x000E, arg1: 0x003C, arg2: 0x003C
@@ -228,7 +199,7 @@ Widget event, handle: 0x0007, signal: 0x0009, arg1: 0x0057, arg2: 0x000D
 		msfw_menubar_set(appid, hellobar, 5);
 
 		/* Not sure how newline is actually represented in buffer inside mailstation */
-		//strcpy(textbuf, "This is\ra long\rmulti-line string\r in an attempt\rto try scrolling\rpenis");
+		//strcpy(histbuf, "This is\ra long\rmulti-line string\r in an attempt\rto try scrolling\rpenis");
 
 		/*XXX:  Is there a way to request a larger memory area for this? I don't think ramdisk_* provides a buffer but rather
 		 * a handle to the buffer */
@@ -236,18 +207,18 @@ Widget event, handle: 0x0007, signal: 0x0009, arg1: 0x0057, arg2: 0x000D
 		/* Is there a read-only flag like inbox email? */
 		//text_history_wid = msfw_widget_new(WID_TEXTENTRY, F_CANFOCUS|F_TEXT_MLINE_SPELL|F_BORDER, 5, 5, 315, 55, appid, 0, 0);
 		//msfw_widget_focus_set(text_history_wid);
-		//msfw_widget_event_handle(text_history_wid, S_TEXT_SETBUF, (uint16_t)textbuf, sizeof(textbuf));
+		//msfw_widget_event_handle(text_history_wid, S_TEXT_SETBUF, (uint16_t)histbuf, sizeof(histbuf));
 		/* Why the fuck does this widget do its own thing and not render where I expect it? */
 		text_input_wid = msfw_widget_new(WID_TEXTENTRY, F_TEXT_RO|F_BORDER, 5, 55, 315, 62, appid, 1, 1);
 		/* Buf needs to be no longer than the length of the textbox, otherwise issues with render */
-		strcpy(textbuf, "A string");
-		msfw_widget_event_handle(text_input_wid, S_TEXT_SETBUF, (uint16_t)textbuf, sizeof(textbuf));
+		strcpy(histbuf, "A string");
+		msfw_widget_event_handle(text_input_wid, S_TEXT_SETBUF, (uint16_t)histbuf, sizeof(histbuf));
 		msfw_widget_focus_set(text_input_wid);
 		/* XXX Check out CFp07/08 09? for inbox app use. Need to know what widget is being used for read only string log */
 		//text_input_wid = msfw_widget_new(WID_RESOURCE, F_RES_STRING, 0, 5, 320, 55, appid, 1, 1);
 		/* below doesn't work, figure that out */
 		//textwidget = msfw_widget_new(WID_STRING, F_STRING, 0, 5, 320, 55, appid, 1, 1);
-		//msfw_widget_event_handle(textwidget, S_TEXT_SETBUF, (uint16_t)textbuf, sizeof(textbuf));
+		//msfw_widget_event_handle(textwidget, S_TEXT_SETBUF, (uint16_t)histbuf, sizeof(histbuf));
 		//msfw_widget_focus_set(textwidget);
 #endif
 
@@ -278,12 +249,19 @@ Widget event, handle: 0x0007, signal: 0x0009, arg1: 0x0057, arg2: 0x000D
 			msfw_led_set(0);
 			msfw_event_put_goprev();
 			break;
-#if 0
 		  case SC_ENTER:
-			msfw_led_set(1);
+			memset(linebuf, '\0', sizeof(linebuf));
+			msfw_widget_event_handle(line_wid, S_TEXT_SETBUF, (uint16_t)linebuf, sizeof(linebuf));
+			msfw_widget_event_handle(line_wid, S_TEXT_REFORMAT, 0, 0);
+			/* Send a backspace event to the wid to cause it to redraw the empty buffer.
+			 * Strangely, sending SIG_DRAW doesn't actually clear the current buf*/
+			msfw_widget_event_handle(line_wid, SIG_KEYDN, SC_BKSP, 0);
+			msfw_widget_event_handle(line_wid, SIG_KEYUP, SC_BKSP, 0);
+			break;
+#if 0
 			/* Clearing the buffer then re-setting it does *sort of* do what I want */
-			memset(textbuf, '\0', sizeof(textbuf));
-			//msfw_widget_event_handle(text_input_wid, S_TEXT_SETBUF, (uint16_t)textbuf2, sizeof(textbuf2));
+			memset(histbuf, '\0', sizeof(histbuf));
+			//msfw_widget_event_handle(text_input_wid, S_TEXT_SETBUF, (uint16_t)histbuf2, sizeof(histbuf2));
 			//msfw_widget_event_handle(text_input_wid, S_TEXT_REFORMAT, 1, 1);
 			//msfw_widget_event_handle(text_input_wid, SIG_DRAW, 0, 0);
 			/* is there a way to tell SOMEONE to redraw the contents of the widget? */
@@ -291,7 +269,7 @@ Widget event, handle: 0x0007, signal: 0x0009, arg1: 0x0057, arg2: 0x000D
 			msfw_msgbox_destroy(text_input_wid);
 			text_input_wid = msfw_widget_new(WID_TEXTENTRY, F_TEXT_ALPHA|F_BORDER, 5, 55, 315, 62, appid, 1, 1);
 			/* Buf needs to be no longer than the length of the textbox, otherwise issues with render */
-			msfw_widget_event_handle(text_input_wid, S_TEXT_SETBUF, (uint16_t)textbuf, sizeof(textbuf));
+			msfw_widget_event_handle(text_input_wid, S_TEXT_SETBUF, (uint16_t)histbuf, sizeof(histbuf));
 			msfw_widget_focus_set(text_input_wid);
 			//msfw_widget_event_handle(text_input_wid, S_TEXT_SETCUR, 0, 0);
 			break;
@@ -315,7 +293,7 @@ Widget event, handle: 0x0007, signal: 0x0009, arg1: 0x0057, arg2: 0x000D
 
 			memset(buf, '\0', 20);
 			memcpy(buf, "test", 4);
-			//msfw_widget_event_handle(textwidget, S_TEXT_CAT, (uint16_t)buf, sizeof(textbuf));
+			//msfw_widget_event_handle(textwidget, S_TEXT_CAT, (uint16_t)buf, sizeof(histbuf));
 			//msfw_widget_event_handle(textwidget, SIG_DRAW, (uint16_t)buf, (uint16_t)buf);
 			;
 			break;
@@ -324,20 +302,7 @@ Widget event, handle: 0x0007, signal: 0x0009, arg1: 0x0057, arg2: 0x000D
 			//progbar = msfw_msgbox_progbar(STR_IDX_APP00, STR_IDX_APP09);
 			//msfw_widget_event_handle(progbar, sig_pdlg_status, STR_IDX_APP11, 0);
 			//timer = msfw_timer_new(appid, 1000, 1);
-			timeout = 0;
 			break;
-		  case MENU_F4:
-			msfw_msgbox_new(STR_IDX_APP03, MSG_CAN_OK);
-			break;
-		  case MENU_F5:
-			memcpy(hellobar, &menudata[5], sizeof(hellobar));
-			msfw_menubar_set(appid, hellobar, 5);
-			break;
-		  case MENU_F1_2:
-			memcpy(hellobar, menudata, sizeof(hellobar));
-			msfw_menubar_set(appid, hellobar, 5);
-			break;
-
 		  default:
 			break;
 		}
