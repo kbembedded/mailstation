084d23e1eb24c1cd2a61b2eb3bdaecc17525f425
Kris Bahnsen <Kris@KBEmbedded.com>
2021-08-15 16:55:44 -0700

    mslib: Rename parport to msfw_parport
    To prevent confusion and issues when adding wifistation support

--- src/include/msfw_parport.h
+++ src/include/msfw_parport.h
@@ -1,10 +1,10 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /* Copyright (c) 2020 KBEmbedded */
 
-#ifndef __PARPORT_H__
-#define __PARPORT_H__
+#ifndef __MSFW_PARPORT_H__
+#define __MSFW_PARPORT_H__
 
 uint16_t msfw_parport_read_byte(void);
 uint8_t msfw_parport_write_byte(uint8_t val);
 
-#endif // __PARPORT_H__
+#endif // __MSFW_PARPORT_H__
