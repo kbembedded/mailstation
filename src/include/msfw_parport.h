3c4b3ee548414989afcb1bc290f53d6c2073a986
Kris Bahnsen <Kris@KBEmbedded.com>
2021-08-15 21:11:17 -0700

    include: msfw_parport: Fix incorrect type

--- src/include/msfw_parport.h
+++ src/include/msfw_parport.h
@@ -5,6 +5,6 @@
 #define __MSFW_PARPORT_H__
 
 uint16_t msfw_parport_read_byte(void);
-uint8_t msfw_parport_write_byte(uint8_t val);
+uint16_t msfw_parport_write_byte(uint8_t val);
 
 #endif // __MSFW_PARPORT_H__
